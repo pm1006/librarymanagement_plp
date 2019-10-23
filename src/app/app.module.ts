6f5eae953332c59d4d5dd7cf8079a5c812e70523
pm1006 <pm6713560@gmail.com>
2019-10-23 18:18:32 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { RegisterStudentComponent } from './register-student/register-student.component';
+import { AdminActivitiesComponent } from './admin-activities/admin-activities.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    RegisterStudentComponent,
+    AdminActivitiesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path:'',component:HomeComponent},
+      {path:'login',component:LoginComponent},
+    ]),
+      ReactiveFormsModule,
+      HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user.ts
@@ -0,0 +1,7 @@
+export class User{
+    userId:string;
+    userName:string;
+    emailId:string;
+    password:string;
+    type:string;
+}
